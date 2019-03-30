7e11713553308ea3a7de2b2345daee449295cd80
Koji Sugimoto <kouji360@KojinoMacBook-Pro.local>
2019-03-30 16:32:01 +0900

    init

---
+++ youtube-ranking-api/src/main/java/wshimomura/back/youtuberankingapi/YoutubeRankingApiApplication.java
@@ -0,0 +1,13 @@
+package wshimomura.back.youtuberankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeRankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeRankingApiApplication.class, args);
+	}
+
+}
